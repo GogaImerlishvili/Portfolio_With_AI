623c82547f1ba99d6a4ae8db5b02c2f9abfbb21c
Goga Imerlishvili <gogaimerlishvili1@gmail.com>
2025-03-24 14:36:01 +0400

    add span tag

--- src/app/Footer/index.tsx
+++ src/app/Footer/index.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
     return (
       <footer className="p-4 bg-gray-800 text-white text-center">
         <p>© 2023 My Portfolio. All rights reserved.</p>
+        <span>Span</span>
       </footer>
     );
   }
